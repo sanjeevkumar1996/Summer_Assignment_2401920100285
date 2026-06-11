68df5b5c18a5c8f459dcb5f8b0ff45b0012f52fc
Sanjeev Kumar <sk6112636@gmail.com>
2026-06-11 22:36:40 +0530

    Week_2  Day_4 solutions

---
+++ Week_2/Day 4/ReverseStringIII.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+
+        int start = 0;
+        int n = s.length();
+
+        for (int i = 0; i <= n; i++) {
+
+            if (i == n || s[i] == ' ') {
+
+                int left = start;
+                int right = i - 1;
+
+                while (left < right) {
+                    swap(s[left], s[right]);
+                    left++;
+                    right--;
+                }
+
+                start = i + 1;
+            }
+        }
+
+        return s;
+    }
+};
